0888c2af33b2a6f2db2d7d72aa990980638f5d87
greedman231 <93518502+greedman231@users.noreply.github.com>
2021-11-02 13:43:43 +0300

    T-0
    1

---
+++ Новая папка/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+        var1 = 4096;
+        var2 = var1 / 4;
+        System.out.print("var = " + var1);
+        System.out.print("var2 = vra1 / 4" + var2);
+    }
+}
